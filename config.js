d9d4debfeeaee62737ae9a897bd4229272845f0b
parthdhwajendramishra <parthdhwajendramishra@gmail.com>
2020-07-03 20:09:57 +0530

    Uploading project

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 let config = {
-    host    : 'localhost',
-    user    : 'root',
-    password: '',
-    database: 'formrecord'
+    host    : '50.62.209.3',
+    port    :'3306',
+    user    : 'ereado',
+    password: '6*ipo3Z9',
+    database: 'eReado_DB'
   };
   
   module.exports = config;
